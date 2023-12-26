36be65d06867bf30aa7f35414a9191bf4175e2ae
brad467388 <dream010436@gmail.com>
2023-12-26 21:44:14 +0800

    完成主要遊戲邏輯

--- game.py
+++ game.py
@@ -1,6 +1,20 @@
 import random
 
+
 # 1~50
-for i in range(10):
-    x = random.randint(1, 50)
-    print(x)
+# 使用者 猜五次
+# 猜對 提前離開
+# 猜錯 提示答案
+
+# 題目
+x = random.randint(1, 50)
+print(x)
+
+
+for i in range(5):
+    y = eval(input('請猜數字(1,50): '))
+    if y == x:
+        print('猜對了！')
+        break
+    else:
+        print('猜錯了 ~~ ')
