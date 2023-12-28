d107a6af27c28cf379c6d5dc2f0703f55664e713
brad467388 <dream010436@gmail.com>
2023-12-28 20:33:57 +0800

    猜數字的區間提示功能

--- game.py
+++ game.py
@@ -7,16 +7,35 @@ import random
 # 猜錯 提示答案
 
 # 題目
-x = random.randint(1, 50)
-print(x)
+# 提示區間(1~50)->(1~25)....
+
+start = 1
+end = 50
+
+x = random.randint(start, end)
+print(f"範圍:{start},{end}")
+# print(x)
 
 
 for i in range(5):
-    y = eval(input('請猜數字(1,50): '))
-    if y == x:
-        print('猜對了！')
-        break
-    elif y < x:
-        print("猜錯了 比 %2d 大" % y)
-    elif y > x:
-        print("猜錯了 比 %2d 小" % y)
+    y = eval(input(f'請猜數字({start},{end}):'))
+    if i == 4:
+        print('次數用完了')
+    else:
+        if y == x:
+            print('猜對了！')
+            break
+        else:
+            if y > x:
+                print('猜低一點')
+                if end > y:
+                    end = y - 1
+            else:
+                if start < y:
+                    start = y + 1
+                print('猜高一點')
+
+if y != x:
+    print(f"答案為:{x}")
+else:
+    print(f"恭喜過關,共猜{i+1}次")
